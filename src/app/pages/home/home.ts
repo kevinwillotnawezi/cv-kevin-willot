d75e01f5485d1d0ab6253df88481cf73eb5c71cf
Kevin Willot <66628344+kevinwillotnawezi@users.noreply.github.com>
2025-10-08 23:55:02 +0200

    Ajout des composants Intro, Experiences, Education et Skills dans le template de la page d'accueil.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Intro } from "./intro/intro";
+import { Experiences } from "./experiences/experiences";
+import { Education } from "./education/education";
+import { Skills } from "./skills/skills";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Intro, Experiences, Education, Skills],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
