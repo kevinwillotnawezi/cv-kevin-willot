1ad1675baa3288e2eef0e2217104d0186b8fc82c
Kevin Willot <66628344+kevinwillotnawezi@users.noreply.github.com>
2025-10-08 16:50:09 +0200

    Ajout des composants Home, About et NotFound avec leurs routes associées, mise à jour de l'importation des composants dans app.ts et correction du titre dans index.html.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export enum ROUTES {
+  HOME = 'home',
+  ABOUT = 'about',
+  NOT_FOUND = 'not-found'
+}
+
+export const routes: Routes = [
+  {
+    path: ROUTES.HOME,
+    component: Home,
+    title: 'Home'
+  },
+  {
+    path: ROUTES.ABOUT,
+    component: About,
+    title: 'About'
+  },
+  {
+    path: ROUTES.NOT_FOUND,
+    component: NotFound,
+    title: 'Not found'
+  },
+  { path: '', redirectTo: ROUTES.HOME, pathMatch: 'full' },
+  { path: '**', redirectTo: ROUTES.NOT_FOUND },
+];
